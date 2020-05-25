c64a7544344cf29edab795860d68659564d10557
gilbert-kiana <gilbertintabo@gmail.com>
2020-05-25 11:32:08 +0300

    now the buttons work but not that efficiently

--- GIT/directional.js
+++ GIT/directional.js
@@ -1,3 +1,34 @@
-function up(
-  alert("kaiser");
-)
+//function up
+
+function up(){
+  var pos =0;
+  var box =document.getElementById("box");
+  pos +=10;
+ box.style.bottom=pos+"px";
+}
+//function RIGHT
+
+function right(){
+  var pos =0;
+  var box =document.getElementById("box");
+  pos +=10;
+ box.style.left=pos+"px";
+}
+
+//function LEFT
+
+function left(){
+  var pos =0;
+  var box =document.getElementById("box");
+  pos +=10;
+ box.style.right=pos+"px";
+}
+
+//function DOWN
+
+function down(){
+  var pos =0;
+  var box =document.getElementById("box");
+  pos +=10;
+ box.style.top=pos+"px";
+}
